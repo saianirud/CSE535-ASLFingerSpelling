3d62a701ef2d782d305afd03c99135aab86483df
Sai Anirud <saianirud>
2022-04-19 12:12:50 -0700

    requirements

---
+++ dummy.py
@@ -0,0 +1,5 @@
+from alphabet_mode_main import predict_labels_from_frames
+    
+arrPred = predict_labels_from_frames('D:\Academics\MS\Mobile Computing\Project\CSE535-ASLFingerSpelling\\asl_alphabet_test')
+print(arrPred)
+    
