28cc466b4085b4c0eca71d1ad1b6fd7608932057
Sai Anirud <saianirud>
2022-04-22 17:40:00 -0700

    letters using cnn

--- ASL_letters.py
+++ ASL_letters.py
@@ -1,14 +1,11 @@
 import os
 import shutil
-from pandas import DataFrame
-from sklearn.metrics import classification_report
-from statistics import mode
-from collections import Counter
-from alphabet_mode_main import predict_labels_from_frames
+from pathlib import Path
 
 PATH_TO_VIDEOS = './Letters/Videos'
 PATH_TO_FRAMES = './Letters/Frames'
 PATH_TO_HAND_FRAMES = './Letters/Hand_Frames'
+PATH_TO_COMBINED_HAND_FRAMES = './Letters/Combined_Hand_Frames'
 PATH_TO_RESULTS = './Letters/results.csv'
 
 
@@ -24,20 +21,37 @@ def clean_dirs():
         for d in dirs:
             shutil.rmtree(os.path.join(root, d))
 
+def generate_posenet_keypoints():
+    for root, dirs, files in os.walk(PATH_TO_VIDEOS):
+        for dir in dirs:
+            path_to_videos, path_to_frames = os.path.join(PATH_TO_VIDEOS, dir), os.path.join(PATH_TO_FRAMES, dir)
+            os.system('python ./posenet/Frames_Extractor.py --path_to_videos=%s --path_to_frames=%s' % (path_to_videos, path_to_frames))
+            os.system('node ./posenet/scale_to_videos.js %s' % (path_to_frames))
+            os.system('python ./posenet/convert_to_csv.py --path_to_videos=%s --path_to_frames=%s' % (path_to_videos, path_to_frames))
 
-# get List of unhidden files
-def listdir_nohidden(path):
-    for f in os.listdir(path):
-        if not f.startswith('.'):
-            yield f
+def extract_hand_frames():
+    for root, dirs, files in os.walk(PATH_TO_VIDEOS):
+        for video in files:
+            path_to_file = os.path.join(root, video)
+            path_to_frames, path_to_hand_frames = os.path.join(PATH_TO_FRAMES, Path(path_to_file).parent.name), os.path.join(PATH_TO_HAND_FRAMES, Path(path_to_file).parent.name)
+            video_name = video.split('.')[0]
+            print('Test video ' + path_to_file + ' loaded')
+            
+            os.system('python ./hand_extractor/hand_extractor.py --source=%s --video=%s --frame_path=%s' % (path_to_file, video_name, path_to_hand_frames))
+            # os.system('python ./hand_extractor.py --path_to_frames=%s --path_to_hand_frames=%s' % (path_to_frames + '/' + video_name, path_to_hand_frames + '/' + video_name))
 
-def final_prediction(pred):
-    """ Returns the most common label from video frames as the final prediction """
-    # print( pred )
-    if not pred or len(pred) == 0:
-        return ' '
-    pred_final = Counter(pred).most_common(1)[0][0]
-    return pred_final
+def generate_combined_frames():
+    alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    for alphabet in alphabets:
+        count = 0
+        if not os.path.exists(PATH_TO_COMBINED_HAND_FRAMES + '/' + alphabet):
+            os.makedirs(PATH_TO_COMBINED_HAND_FRAMES + '/' + alphabet)
+        for root, dirs, files in os.walk(PATH_TO_VIDEOS):
+            for dir in dirs:
+                for r, d, fls in os.walk(os.path.join(PATH_TO_HAND_FRAMES, dir, alphabet)):
+                    for f in fls:
+                        shutil.copy2(os.path.join(r, f), PATH_TO_COMBINED_HAND_FRAMES + '/' + alphabet + '/' + str(count) + '.png')
+                        count += 1
 
 
 
@@ -48,41 +62,20 @@ if not os.path.exists(PATH_TO_FRAMES):
     os.makedirs(PATH_TO_FRAMES)
 if not os.path.exists(PATH_TO_HAND_FRAMES):
     os.makedirs(PATH_TO_HAND_FRAMES)
+if not os.path.exists(PATH_TO_COMBINED_HAND_FRAMES):
+    os.makedirs(PATH_TO_COMBINED_HAND_FRAMES)
 
-# Initialise the prediction array
-arrPred = []
+# Create posenet wrist points
+generate_posenet_keypoints()
 
-os.system('python ./posenet/Frames_Extractor.py --path_to_videos=%s --path_to_frames=%s' % (PATH_TO_VIDEOS, PATH_TO_FRAMES))
-os.system('node ./posenet/scale_to_videos.js %s' % (PATH_TO_FRAMES))
-os.system('python ./posenet/convert_to_csv.py --path_to_videos=%s --path_to_frames=%s' % (PATH_TO_VIDEOS, PATH_TO_FRAMES))
+# extract hand frames from videos
+extract_hand_frames()
 
-# Get list of test videos
-list_of_videos = listdir_nohidden(PATH_TO_VIDEOS)
+# combined hand frames of each letter into one folder
+generate_combined_frames()
 
-# Initialise predicted array
-predicted = []
+# train model on kaggle dataset
+os.system('python ./cnn_model.py --path_to_dataset=%s --save_model=%s' % ('./asl-alphabet/asl_alphabet_train/', 'cnn.h5'))
 
-for video in list_of_videos:
-
-    path_to_file = PATH_TO_VIDEOS + '/' + video
-    video_name = video.split('.')[0]
-    print('Test video ' + video + ' loaded')
-    
-    os.system('python ./hand_extractor/hand_extractor.py --source=%s --video=%s --frame_path=%s' % (path_to_file, video_name, PATH_TO_HAND_FRAMES))
-    # os.system('python ./hand_extractor.py --path_to_frames=%s --path_to_hand_frames=%s' % (PATH_TO_FRAMES + '/' + video_name, PATH_TO_HAND_FRAMES + '/' + video_name))
-    
-    arrPred = predict_labels_from_frames(PATH_TO_HAND_FRAMES + '/' + video_name)
-    
-    # Calculate Prediction and handle none cases
-    try:
-        valPred = mode(arrPred)
-    except:
-        valPred = ''
-    
-    actualLabel = video_name
-    print("\nActual Value: " + actualLabel + " Predicted Value: " + valPred)
-    predicted.append([valPred, actualLabel])
-
-df = DataFrame (predicted, columns=['predicted', 'actual'])
-print(classification_report(df.predicted, df.actual))
-df.to_csv(PATH_TO_RESULTS)
+# finetune model on our dataset
+os.system('python ./cnn_model.py --path_to_dataset=%s --save_model=%s --load_model=%s' % (PATH_TO_COMBINED_HAND_FRAMES + '/', 'cnn_model.h5', 'cnn.h5'))
