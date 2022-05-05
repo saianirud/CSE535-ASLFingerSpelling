55c62edb02b78edb908193f154b5c807a0989f13
Sai Anirud <saianirud>
2022-05-04 21:18:10 -0700

    submission ready code

--- hand_extractor.py
+++ hand_extractor.py
@@ -24,9 +24,11 @@ if __name__ == '__main__':
     if not os.path.isdir(path_to_hand_frames):
         os.mkdir(path_to_hand_frames)
 
+    # get frames from path_to_frames
     frames =  [f for f in os.listdir(path_to_frames) if f.endswith('.png')]
     sorted_frames = sorted(frames, key=lambda x: int(os.path.splitext(x)[0]))
 
+    # posenet key points
     key_points = pd.read_csv(path_to_frames + '/' + 'key_points.csv')
     rightWrist_x = key_points.rightWrist_x
     rightWrist_y = key_points.rightWrist_y
@@ -35,8 +37,13 @@ if __name__ == '__main__':
     for frame in sorted_frames:
         try:
             if i < len(rightWrist_x):
-                img = cv2.imread(path_to_frames + '/' + frame)
-                hand_frame = img[max(round(rightWrist_y[i])-300, 0):round(rightWrist_y[i])+100, max(round(rightWrist_x[i])-200, 0):round(rightWrist_x[i])+200]
+                # cropping the hand part from the frame using posenet wrist points
+                image = cv2.imread(path_to_frames + '/' + frame)
+                hand_frame = image[
+                    max(round(rightWrist_y[i])-300, 0):round(rightWrist_y[i])+100, 
+                    max(round(rightWrist_x[i])-200, 0):round(rightWrist_x[i])+200
+                ]
+                # saving the cropped hand to an image file
                 cv2.imwrite(path_to_hand_frames + '/' + str(i) + '.png', hand_frame)
                 i = i + 1
         except:
